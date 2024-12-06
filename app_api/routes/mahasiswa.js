11c03e59a3cd8d7f2417ae6fba39ce565b448969
MarcelAlpin <marcelalpinsanty_2226240044@mhs.mdp.ac.id>
2024-12-06 11:16:50 +0700

    Update mahasiswa.js

--- app_api/routes/mahasiswa.js
+++ app_api/routes/mahasiswa.js
@@ -1,12 +1,6 @@
 const express = require("express"); // Mengimpor framework Express.js
 const router = express.Router(); // Membuat instance Router dari Express
 const mahasiswaController = require("../controllers/mahasiswaController"); // Mengimpor controller untuk mahasiswa
-const upload = require("../middleware/uploadMiddleware"); // Middleware untuk menangani upload file
-
-// Mengimpor middleware untuk autentikasi dan pengecekan peran
-// const authMiddleware = require("../middleware/authMiddleware");
-// const roleMiddleware = require("../middleware/roleMiddleware");
-
 // Route untuk mendapatkan semua data mahasiswa
 router.get("/", mahasiswaController.getAllMahasiswa);
 // Mengarahkan ke fungsi getAllMahasiswa di controller untuk mengambil semua data mahasiswa
@@ -16,32 +10,15 @@ router.get("/:id", mahasiswaController.getMahasiswaById);
 // Mengarahkan ke fungsi getMahasiswaById di controller untuk mengambil data mahasiswa berdasarkan ID
 
 // Route untuk menambahkan data mahasiswa baru, termasuk upload file foto
-router.post(
-  "/",
-  // authMiddleware,
-  // roleMiddleware("admin"),
-  upload.single("foto"),
-  mahasiswaController.createMahasiswa
-);
+router.post("/", mahasiswaController.createMahasiswa);
 // Middleware `upload.single("foto")` menangani upload file dengan field `foto` sebelum fungsi createMahasiswa dijalankan
 
 // Route untuk memperbarui data mahasiswa, dengan upload foto opsional
-router.put(
-  "/:id",
-  // authMiddleware,
-  // roleMiddleware("admin"),
-  upload.single("foto"),
-  mahasiswaController.updateMahasiswa
-);
+router.put("/:id", mahasiswaController.updateMahasiswa);
 // Middleware `upload.single("foto")` menangani upload file dengan field `foto` sebelum fungsi updateMahasiswa dijalankan
 
 // Route untuk menghapus data mahasiswa berdasarkan ID
-router.delete(
-  "/:id",
-  // authMiddleware,
-  // roleMiddleware("admin"),
-  mahasiswaController.deleteMahasiswa
-);
+router.delete("/:id", mahasiswaController.deleteMahasiswa);
 // Mengarahkan ke fungsi deleteMahasiswa di controller untuk menghapus data mahasiswa berdasarkan ID
 
 module.exports = router; // Mengekspor router agar dapat digunakan di file lain
