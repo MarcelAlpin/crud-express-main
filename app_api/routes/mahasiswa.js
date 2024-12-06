469718ec8596ba27e8f169a3358ffbbda828c680
MarcelAlpin <marcelalpinsanty_2226240044@mhs.mdp.ac.id>
2024-12-06 11:18:11 +0700

    Update mahasiswa.js

--- app_api/routes/mahasiswa.js
+++ app_api/routes/mahasiswa.js
@@ -10,11 +10,11 @@ router.get("/:id", mahasiswaController.getMahasiswaById);
 // Mengarahkan ke fungsi getMahasiswaById di controller untuk mengambil data mahasiswa berdasarkan ID
 
 // Route untuk menambahkan data mahasiswa baru, termasuk upload file foto
-router.post("/", mahasiswaController.createMahasiswa);
+router.post("/", upload.single("foto"), mahasiswaController.createMahasiswa);
 // Middleware `upload.single("foto")` menangani upload file dengan field `foto` sebelum fungsi createMahasiswa dijalankan
 
 // Route untuk memperbarui data mahasiswa, dengan upload foto opsional
-router.put("/:id", mahasiswaController.updateMahasiswa);
+router.put("/:id", upload.single("foto"), mahasiswaController.updateMahasiswa);
 // Middleware `upload.single("foto")` menangani upload file dengan field `foto` sebelum fungsi updateMahasiswa dijalankan
 
 // Route untuk menghapus data mahasiswa berdasarkan ID
